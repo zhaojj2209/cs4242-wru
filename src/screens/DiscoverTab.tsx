5faa6f21065e587a36a5cfaa05f8415bab03f7fe
zhaojj2209 <zhaojj2000@gmail.com>
2023-03-26 00:11:40 +0800

    Add location

--- src/screens/DiscoverTab.tsx
+++ src/screens/DiscoverTab.tsx
@@ -1,11 +1,54 @@
 import { StyleSheet, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MapView from 'react-native-maps'
+import { getCurrentPositionAsync, requestForegroundPermissionsAsync } from 'expo-location'
+import { ActivityIndicator } from 'react-native-paper'
 
 const DiscoverTab = () => {
+  const sgCoords = {
+    // Taken from google
+    latitude: 1.3521,
+    longitude: 103.8198,
+    // Rough estimate to fit entire Sg map
+    latitudeDelta: 0.8,
+    longitudeDelta: 0.1,
+  }
+  const [coords, setCoords] = useState(sgCoords)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    (async () => {
+      
+      const { status } = await requestForegroundPermissionsAsync()
+      if (status !== 'granted') {
+        setLoading(false)
+        return
+      }
+
+      const location = await getCurrentPositionAsync({})
+      setCoords({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      })
+      setLoading(false)
+    })()
+  }, [])
+
   return (
     <View style={styles.container}>
-      <MapView provider="google" style={styles.map} />
+      {loading ? (
+        <ActivityIndicator />
+      ) : (
+        <MapView
+          provider="google"
+          style={styles.map}
+          initialRegion={coords}
+          showsUserLocation
+          loadingEnabled
+        />
+      )}
     </View>
   )
 }
@@ -15,6 +58,8 @@ export default DiscoverTab
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   map: {
     width: '100%',
